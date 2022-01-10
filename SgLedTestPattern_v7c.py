b0a220c0bad079f886eb3c9f99d4f19c9e9d0d19
Sean <macswg@gmail.com>
2022-01-10 11:53:18 -0500

    This is the updated version 7c

--- SgLedTestPattern_v7c.py
+++ SgLedTestPattern_v7c.py
@@ -11,7 +11,7 @@ logging.basicConfig(
 )
 
 # disables logging
-#logging.disable(logging.CRITICAL)
+# logging.disable(logging.CRITICAL)
 
 # Import pillow image module
 from PIL import Image, ImageDraw, ImageFont, ImageColor
@@ -45,8 +45,10 @@ def int_input_validation(prompt):
     Returns False if there is not a row of half panels.
     Also returns tile height number
 '''
+
+
 def half_tile_check(i):
-    error1 = '\nThat is not a valid panel height\nEither use a whole number or add .5 for half panel\n' 
+    error1 = '\nThat is not a valid panel height\nEither use a whole number or add .5 for half panel\n'
     while True:
         try:
             TILECOUNT = input(i)
@@ -66,8 +68,6 @@ def half_tile_check(i):
             i = TILECOUNT
             break
     return x, int(i)
-
-
 
 
 # Function to validate the background color
@@ -95,15 +95,19 @@ def color_input_validation():
 def even_is_true(i):
     i = int(i)
     if (i % 2) == 0:
-        x = True 
+        x = True
     else:
         x = False
-    return x 
+    return x
 
 
 # Ask for user input of LED tile dimensions and bg color
-tileResWidth = int_input_validation('\n' + 'What is the tile width (horizontal resolution)? ')
-tileResHeight = int_input_validation('\n' + 'What is the tile height (vertical resolution)? ')
+tileResWidth = int_input_validation(
+    '\n' + 'What is the tile width (horizontal resolution)? '
+)
+tileResHeight = int_input_validation(
+    '\n' + 'What is the tile height (vertical resolution)? '
+)
 
 ''' color is asked for in the color_input_validation function because
     I don't know how else to verify value is correct
@@ -170,15 +174,17 @@ makeBorder(ledIm2)
 # Ask for user input of wall dimensions
 logging.debug('Start tiling panels onto wall pattern')
 wallPanelWidth = int_input_validation(
-    '\n' + 'How many tiles wide do you need the pattern? ')
+    '\n' + 'How many tiles wide do you need the pattern? '
+)
 half_tile_bool, wallPanelHeight = half_tile_check(
-    '\n' + 'How many tiles high do you need the pattern? (half-tiles are ok) ')
+    '\n' + 'How many tiles high do you need the pattern? (half-tiles are ok) '
+)
 
 
 if half_tile_bool == True:
     logging.debug('half tile is TRUE')
-    ledIm3_half = Image.new('RGBA', (tileResWidth, int(tileResHeight/2)), bgColor)
-    ledIm4_half = Image.new('RGBA', (tileResWidth, int(tileResHeight/2)), bgColor2)
+    ledIm3_half = Image.new('RGBA', (tileResWidth, int(tileResHeight / 2)), bgColor)
+    ledIm4_half = Image.new('RGBA', (tileResWidth, int(tileResHeight / 2)), bgColor2)
     makeBorder(ledIm3_half)
     makeBorder(ledIm4_half)
 else:
@@ -190,14 +196,20 @@ logging.debug('Program continues after half_tile_bool')
 # Create new image at size of wall
 if half_tile_bool == True:
     wallIm = Image.new(
-        'RGBA', (wallPanelWidth * tileResWidth, (wallPanelHeight * tileResHeight)
-         + int(tileResHeight/2)))
+        'RGBA',
+        (
+            wallPanelWidth * tileResWidth,
+            (wallPanelHeight * tileResHeight) + int(tileResHeight / 2),
+        ),
+    )
 else:
     wallIm = Image.new(
-        'RGBA', (wallPanelWidth * tileResWidth, wallPanelHeight * tileResHeight))
+        'RGBA', (wallPanelWidth * tileResWidth, wallPanelHeight * tileResHeight)
+    )
 
 logging.debug('wallIm size = ' + str(wallIm.size))
 logging.debug('wallPanelWidth 118 = ' + str(wallPanelWidth))
+logging.debug('wallPanelHeight = ' + str(wallPanelHeight))
 logging.debug('tileResWidth 119 = ' + str(tileResWidth))
 logging.debug('tileResHeight 120 = ' + str(tileResHeight))
 
@@ -208,6 +220,9 @@ tileResWidth, tileResHeight = ledIm.size
 logging.debug('wallPanelWidth = ' + str(wallPanelWidth))
 logging.debug('tileResWidth = ' + str(tileResWidth))
 logging.debug('Start for loop to copy panel images with alternating colors')
+
+logging.debug('wallPanelHeight begin loop at 213 = ' + str(wallPanelHeight))
+logging.debug('wallPanelHeight = ' + str(wallPanelHeight2))
 
 for left in range(0, wallPanelWidth2, tileResWidth * 2):
     for top in range(0, wallPanelHeight2, tileResHeight * 2):
@@ -222,7 +237,10 @@ for leftAlt in range(tileResWidth, wallPanelWidth2, tileResWidth * 2):
     for topAlt in range(0, wallPanelHeight2, tileResHeight * 2):
         wallIm.paste(ledIm2, (leftAlt, topAlt))
 
-# If half panels then do this
+logging.debug('wallPanelHeight begin loop at 229 = ' + str(wallPanelHeight))
+logging.debug('wallPanelHeight = ' + str(wallPanelHeight2))
+
+# If half panels then do this - alternates half-panel colors
 if half_tile_bool == True:
     if even_is_true(wallPanelHeight) == True:
         tile_color_1 = ledIm3_half
@@ -231,10 +249,18 @@ if half_tile_bool == True:
         tile_color_1 = ledIm4_half
         tile_color_2 = ledIm3_half
     for left in range(0, wallPanelWidth2, tileResWidth * 2):
-        for top in range((wallPanelHeight2 - int(tileResHeight/2)), wallPanelHeight2, int(tileResHeight/2) * 2):
+        for top in range(
+            (wallPanelHeight2 - int(tileResHeight / 2)),
+            wallPanelHeight2,
+            int(tileResHeight / 2) * 2,
+        ):
             wallIm.paste(tile_color_1, (left, top))
     for leftAlt in range(tileResWidth, wallPanelWidth2, tileResWidth * 2):
-        for topAlt in range((wallPanelHeight2 - (int(tileResHeight/2))), wallPanelHeight2, tileResHeight * 2):
+        for topAlt in range(
+            (wallPanelHeight2 - (int(tileResHeight / 2))),
+            wallPanelHeight2,
+            tileResHeight * 2,
+        ):
             wallIm.paste(tile_color_2, (leftAlt, topAlt))
 
 
@@ -242,20 +268,28 @@ logging.debug('For loop A copying led panels to wall complete')
 logging.debug('tileResWidth 141 = ' + str(tileResWidth))
 logging.debug('tileResHeight 142 = ' + str(tileResHeight))
 
+logging.debug('wallPanelHeight = ' + str(wallPanelHeight))
+logging.debug('wallPanelHeight = ' + str(wallPanelHeight2))
+
 
 # creating variables to loop later - also centering the text
 draw = ImageDraw.Draw(wallIm)
-W, H = (tileResWidth, tileResHeight)
+W, H, half_H = (tileResWidth, tileResHeight, (tileResHeight / 2))
+
+logging.debug('W = ' + str(W) + 'H = ' + str(H) + 'half_H = ' + str(half_H))
 
 # calculate appropriate font size for panel resolution
 fontCal = int(min(tileResHeight, tileResWidth) / 2 * 0.6)
 fontsFolder = 'FONT_FOLDER'
 arialFont = ImageFont.truetype(os.path.join(fontsFolder, 'arial.ttf'), fontCal)
 
-# these variables are defined outside the loops
-# so they can be manipulated
-# List (needs to be converted to string for use)
+''' these variables are defined outside the loops
+    so they can be manipulated
+    List (needs to be converted to string for use)'''
 indexNums = [1, 1]
+
+
+# logging.debug('halftile_indexNums = ' + str(halftile_indexNums))
 
 # This function converts list to string for use in draw.text lines
 def iNc(i):
@@ -272,6 +306,9 @@ adjusted_x_coord_for_text, adjusted_y_coord_for_text = (
     CENT_X_CORD_FOR_TEXT,
     CENT_Y_CORD_FOR_TEXT,
 )
+if half_tile_bool == True:
+    CENT_half_Y_CORD_FOR_TEXT = (half_H - h) / 2
+    adjusted_y_coord_for_text_halfpanel = CENT_half_Y_CORD_FOR_TEXT
 
 # TODO: Deduplicate some of this code
 
@@ -280,13 +317,22 @@ while True:
     logging.debug('Start while true')
     loop_counter1 = 0
 
-    while indexNums[0] <= wallPanelWidth + 1:
+    while indexNums[0] <= wallPanelWidth:
 
         loop_counter1 += 1
-        logging.debug('While 1 statement start ' + str(loop_counter1))
+        logging.debug(
+            'While 1 statement start. ' + str(loop_counter1) + str(' ') + str(indexNums)
+        )
 
         adjusted_x_coord_for_text = ((W - w) / 2) + (indexNums[0] - 1) * tileResWidth
         adjusted_y_coord_for_text = ((H - h) / 2) + (indexNums[1] - 1) * tileResHeight
+
+        # adds more text to half panels if they exist
+        if half_tile_bool == True:
+            adjusted_x_coord_for_text_halfpanel = \
+                ((W - w) / 2) + (indexNums[0] - 1) * tileResWidth
+            adjusted_y_coord_for_text_halfpanel = \
+                ((half_H - h) / 2) + (indexNums[1]) * (tileResHeight)
 
         # calculates the width and height of text to be drawn
         w, h = draw.textsize(iNc(indexNums), font=arialFont)
@@ -299,27 +345,77 @@ while True:
             font=arialFont,
         )
 
+        # # This if statements draws the half-tile numbers
+        # if half_tile_bool == True:
+        #     logging.debug('Begin drawing half-tile text ')
+        #     # draws text on the half panel
+        #     draw.text(
+        #         (
+        #             adjusted_x_coord_for_text_halfpanel,
+        #             adjusted_y_coord_for_text_halfpanel,
+        #         ),
+        #         iNc(indexNums),
+        #         fill='gray',
+        #         font=arialFont,
+        #     )
+        #     logging.debug('END IF statement drawing half-tile text ' + str(indexNums))
+
+            # logging.debug('halftile_indexNums = ' + str(halftile_indexNums))
+
         # adds to index number
         indexNums[1] += 1
+
+        logging.debug('indexNums update 370 ' +str(indexNums))
 
         # calculates the width and height of text to be drawn
         w, h = draw.textsize(iNc(indexNums), font=arialFont)
 
+        # This if statements draws the half-tile numbers
+        if half_tile_bool == True:
+            logging.debug('Begin drawing half-tile text ')
+
+            # TODO: Some way to only draw if tile is half width?
+            if indexNums[1] == wallPanelHeight + 1:
+                # draws text on the half panel
+                draw.text(
+                    (
+                        adjusted_x_coord_for_text_halfpanel,
+                        adjusted_y_coord_for_text_halfpanel,
+                    ),
+                    iNc(indexNums),
+                    fill='gray',
+                    font=arialFont,
+                )
+            logging.debug('END IF statement LOWER drawing half-tile text ' + str(indexNums))
+            logging.debug('iNc function output = ' + str(iNc(indexNums)))
+            logging.debug('adjusted_x_coord_for_text_halfpanel = ' + str(adjusted_x_coord_for_text_halfpanel))
+            logging.debug('adjusted_y_coord_for_text_halfpanel = ' + str(adjusted_y_coord_for_text_halfpanel))
+            logging.debug('adjusted_x_coord_for_text = ' + str(adjusted_x_coord_for_text))
+            logging.debug('adjusted_y_coord_for_text = ' + str(adjusted_y_coord_for_text) + '\n')
+
+
+
+
+
         # original if statement
         if indexNums[1] == wallPanelHeight + 1:
-            logging.debug('Start if indexNums A')
 
+            logging.debug('Start if indexNums A. ' + str(indexNums))
+
+            # these are necessary to prevent infinite loop
             indexNums[0] += 1
             indexNums[1] = 1
 
-            adjusted_x_coord_for_text = CENT_X_CORD_FOR_TEXT
-            adjusted_y_coord_for_text += tileResHeight
+
+            # adjusted_x_coord_for_text = CENT_X_CORD_FOR_TEXT
+            # adjusted_y_coord_for_text += tileResHeight
 
             # calculates the width and height of text to be drawn
-            w, h = draw.textsize(iNc(indexNums), font=arialFont)
-            logging.debug(
-                'draw.textsize w value = ' + str(w) + '-- h value = ' + str(h)
-            )
+            # w, h = draw.textsize(iNc(indexNums), font=arialFont)
+            # logging.debug(
+            #     'draw.textsize w value = ' + str(w) + '-- h value = ' + str(h)
+            #     + str(indexNums)
+            # )
     break
 
 # TODO: be able to handle half panels
